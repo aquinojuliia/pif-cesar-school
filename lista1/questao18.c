3bcae2f62af455bfcb94aee1c93e56aa380ea63a
aquinojuliia <jal2@cesar.school>
2026-08-20 22:01:11 +0000

    adicione as questao 18 19 20 na lista1

---
+++ lista1/questao18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float Lapis = 4.88;
+    float Borrachas = 234.54;
+    float Canetas = 42.04;
+    float Cadernos = 8.00;
+    float Fitas = 13.05;
+
+     printf("%-12s %8.2f\n", "Lapis", Lapis);
+    printf("%-12s %8.2f\n", "Borrachas", Borrachas);
+    printf("%-12s %8.2f\n", "Canetas", Canetas);
+    printf("%-12s %8.2f\n", "Cadernos", Cadernos);
+    printf("%-12s %8.2f\n", "Fitas", Fitas);
+
+
+    return 0;
+}
